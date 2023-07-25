af0ef5350f6d5dfab31702dbfb7ee21a40c318c1
Viktoria Czaran <vczaran@gmail.com>
2023-07-25 12:19:27 -0400

    Working on navbar dropdown styling

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -5,24 +5,48 @@ import LogOut from "../LogOut";
 
 function DropDown () {
     const [show, setShow] = useState(false);
+    const loggedin = sessionStorage.getItem("currentUser");
 
+    // <button onMouseEnter={() => setShow(true)}
+    //         onMouseLeave={() => setShow(false)}>sign in   
+    // </button>
+
+    // if (show && !loggedin) {
+    //   return (
+    //     <div className="splash-nav-links">
+    //              <Link id="login" to="/login">Sign In</Link>
+    //               <br/>
+    //               <Link id="create" to="/register">Create an Account</Link>
+    //      </div>
+    //   )
+    // } else if (show && loggedin) {
+    //   return (
+    //     <div className="logout-from-dropdown">
+    //            <LogOut />
+    //     </div>
+    //   )
+    // }
     return (
-      <div className="DropDown">
-        <button
-          onMouseEnter={() => setShow(true)}
-          onMouseLeave={() => setShow(false)}>
-          sign in
-        </button>
-        {show && (
-          <div className="splash-nav-links">
-            <Link to="/login">Sign In</Link>
-            <br/>
-            <Link to="/register">Create an Account</Link>
-            <br/>
+      <div className="DropDown"  
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}>sign in
+          {/* <button> 
+            {/* put carrot here */}
+          {/* </button> */}
+          
+          {show && !loggedin && (
+            <div className="splash-nav-links">
+              <Link id="login" to="/login">Sign In</Link>
+              <Link id="create" to="/register">Create an Account</Link>
+            </div>
+          )}
+          {show && loggedin && (
+          <div className="logout-from-dropdown">
             <LogOut />
           </div>
-        )}
-      </div>
+          )}
+          
+        </div>
     );
   }
   
