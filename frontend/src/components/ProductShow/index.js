27f4867ae8b9b0944f96d37adf2e17074b128493
Viktoria Czaran <vczaran@gmail.com>
2023-07-27 16:10:11 -0400

    Added option buttons to show page

--- frontend/src/components/ProductShow/index.js
+++ frontend/src/components/ProductShow/index.js
@@ -17,6 +17,19 @@ function ProductShow () {
         return null
     }
 
+    const FlavorOptions = product.flavorOptions.map(flavor => {
+        return (
+            <button>{flavor}</button>
+        )
+    })
+
+    const SizeOptions = product.sizeOptions.map(size => {
+        return (
+            <button>{size}</button>
+        )
+    })
+
+
     return (
         <>
         <header id="category">{product.category}</header>
@@ -26,31 +39,36 @@ function ProductShow () {
                 <h1>{product.name}</h1>
                 <p>{product.rating}</p>
                 <p id="price">${product.price}</p>
-                <p>{product.flavorOptions}</p>
-                <p>{product.sizeOptions}</p>
+                <div className="flavors">Flavor
+                    {FlavorOptions}
+                </div>
+                <div className="sizes">Size
+                    {SizeOptions}
+                </div>
             </div>
             <div className="dropdown-buttons-container-show">
                 <label>Quantity
-                <select className="quantity-dropdown" name="quantity">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
-                    <option>7</option>
-                    <option>8</option>
-                    <option>9</option>
-                    <option>10</option>
-                    <option>11</option>
-                    <option>12</option>
-                </select>
-                <button className="add-to-cart-from-show">Add to Cart</button>
+                    <select className="quantity-dropdown" name="quantity">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+                        <option>9</option>
+                        <option>10</option>
+                        <option>11</option>
+                        <option>12</option>
+                    </select>
                 </label>
+
+                <button className="add-to-cart-from-show">Add to Cart</button>   
             </div>
         </div>
         <hr className="show-divider"/>
-        <div className="prodcut-show-details">
+        <div className="product-show-details">
                 <h2>About This Item</h2>
                 <p>{product.details}</p>
         </div>
