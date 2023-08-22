74a295f3625e904c8ea1d1068d43abbda4406ed3
Viktoria Czaran <vczaran@gmail.com>
2023-08-22 16:32:20 -0400

    Add carousel skeleton

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,11 +1,15 @@
 import ProductsIndex from '../ProductsIndex';
 import './SplashPage.css';
-// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function SplashPage () {
     return (
         <div className='SplashPage'>
+            <Carousel className='carousel' autoPlay infiniteLoop>  
+                <img src="/images/raven.jpg" />  
+                <img src="/images/playtime.avif" />  
+            </Carousel>
             <h1>Popular Products</h1>
             <ProductsIndex />
         </div>
