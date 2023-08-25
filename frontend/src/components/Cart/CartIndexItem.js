1d4001463c5c8b91b2b5d7be39b36d7f58696142
Viktoria Czaran <vczaran@gmail.com>
2023-08-25 11:36:22 -0400

    Fix cart page styling

--- frontend/src/components/Cart/CartIndexItem.js
+++ frontend/src/components/Cart/CartIndexItem.js
@@ -27,23 +27,24 @@ export default function CartIndexItem (item) {
                 {item?.item.flavor && (<li>Flavor: {item?.item.flavor}</li>)}
                 {item?.item.size && (<li>Size: {item?.item.size}</li>)}
                 {item?.item.color && (<li>Color: {item?.item.color}</li>)}
-                <li>Quantity: {quantity}</li>
             </div>
             <div id="cart-index-buttons">
-                <select className="quantity-dropdown-cartindex" onChange={handleChange} defaultValue={quantity}>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                </select>
+                <label>Quantity:  
+                    <select className="quantity-dropdown-cartindex" onChange={handleChange} defaultValue={quantity}>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                    </select>
+                </label>
                 <button onClick={() => handleRemove(item.item.id)}>Remove</button>
             </div>
         </ul>
