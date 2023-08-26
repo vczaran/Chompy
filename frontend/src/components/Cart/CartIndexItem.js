15710f194b2f0c6767e604ff94a9e2b46c91b47f
Viktoria Czaran <vczaran@gmail.com>
2023-08-26 12:52:57 -0400

    Add link to product show from cart page

--- frontend/src/components/Cart/CartIndexItem.js
+++ frontend/src/components/Cart/CartIndexItem.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { deleteCartItem, removeProduct, updateCartItem } from "../../store/cart";
 import { useState } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function CartIndexItem (item) {
     const products = useSelector(state => state.products);
@@ -22,7 +23,7 @@ export default function CartIndexItem (item) {
         <ul id="cart-index-item">
             <img src={`${product.imageUrl}`}/>
             <div id="cart-info">
-                <li>{product?.name}</li>
+                <Link id="link-to-show-from-product" to={`/products/${product.id}`}><li>{product?.name}</li></Link>
                 <li id="cart-index-price">${product?.price}</li>
                 {item?.item.flavor && (<li>Flavor: {item?.item.flavor}</li>)}
                 {item?.item.size && (<li>Size: {item?.item.size}</li>)}
